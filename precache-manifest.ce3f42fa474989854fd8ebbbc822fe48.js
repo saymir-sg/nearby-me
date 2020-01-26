7106a3507064be49e0c5ade31a73faeadbe51d21
samir <sameer.ghag@thekhelgroup.com>
2020-01-26 15:31:18 +0530

    Updates

---
+++ precache-manifest.ce3f42fa474989854fd8ebbbc822fe48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc0508b01d7b93ae731b21f18dd09d5a",
+    "url": "/nearby-me/index.html"
+  },
+  {
+    "revision": "d58e3919bdfd5733d355",
+    "url": "/nearby-me/static/css/main.ac340de9.chunk.css"
+  },
+  {
+    "revision": "bb040fc0ea5798e96ab6",
+    "url": "/nearby-me/static/js/2.6ec22b07.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/nearby-me/static/js/2.6ec22b07.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d58e3919bdfd5733d355",
+    "url": "/nearby-me/static/js/main.ef868d40.chunk.js"
+  },
+  {
+    "revision": "d81f37c925fc4496b1a8",
+    "url": "/nearby-me/static/js/runtime-main.ab708c76.js"
+  }
+]);
